64206c6e8aaa6147f71f2c44f06c54b765909d1e
Paul Baker <paul.baker@overstock.com>
2015-10-13 14:20:36 -0600

    Including git-credential-gnome-keyring with building because https credentials should be stored securely.

--- git-openssl-experimental.sh
+++ git-openssl-experimental.sh
@@ -11,6 +11,7 @@ sudo apt-get install asciidoc xmlto docbook2x -y
 
 if [ -d "./git" ]; then
   cd ./git
+  git reset --hard HEAD
   git pull origin master
 else
   git clone http://git.kernel.org/pub/scm/git/git.git
@@ -23,7 +24,18 @@ sudo apt-get autoremove -y
 make configure
 auto-apt run ./configure --prefix=/usr
 make all doc info
-echo "#define VERSION \"9:9.9.9-9${USER}0.9\"" >> config.log
 # Builds a package for easy uninstallation. Don't use this
 # for building distribution packages. Purely for local builds.
-sudo checkinstall make install install-doc install-html install-info
+sudo checkinstall --pkgversion "9:9.9.9-9${USER}0.9" make install install-doc install-html install-info
+
+# If the installalation was successful, then let's build the keyring
+# integration that is really helpful for securely storing https credentials
+if [ $? -eq 0 ]; then
+  sudo apt-get install libgnome-keyring-dev -y
+  cd $(find . -type d -name "gnome-keyring")
+  echo "" >> Makefile
+  echo 'install:$(MAIN)' >> Makefile
+  echo '	mv git-credential-gnome-keyring /usr/local/bin' >> Makefile
+  sudo checkinstall --pkgname "git-credential-gnome-keyring" --pkgversion "9:9.9.9-9${USER}0.9" --requires git
+  git config --global credential.helper "/usr/local/bin/git-credential-gnome-keyring"
+fi
