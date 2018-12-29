8fcd30c17f8bf92addb261f7676bbf3a95c7dab9
Paul  Baker <paul.baker@wgu.edu>
2018-12-28 20:52:14 -0700

    Making test script more efficient (but not necessarily by much)

--- test-with-docker.sh
+++ test-with-docker.sh
@@ -1,20 +1,39 @@
 #!/usr/bin/env bash
 
-temp_directory=$(mkdir -d)
-cd "${temp_directory}"
+# Utilize concurrency to build all the docker containers we'll use to test with
+echo "Building testable ubuntu linux images... Fair warning, this will take a LONG time"
+sleep 5s
+declare -a ubuntu_versions=("14.04" "16.04" "18.04" "18.10" "19.04")
+function build_containers()
+{
+  for ubuntu_version in "${ubuntu_versions[@]}"; do
+    dockerfile="ubuntu-${ubuntu_version//\.}"
+    ubuntu_container="ubuntu:${ubuntu_version}-with-sudo"
+    echo -e "${green}Building image ${ubuntu_container} from file ${dockerfile}${no_color}"
+    docker build -t "${ubuntu_container}" -f "dockerfiles/${dockerfile}" ./dockerfiles &
+  done
+  # Wait for all jobs to complete
+  for job_pid in $(jobs -p); do
+    wait "${job_pid}"
+  done
+}
+time build_containers
 
-declare -a ubuntu_versions=("14.04") # "16.04" "18.04" "18.10" "19.04")
+
+# Now working with the images individually so we can test the script on each container
+echo "Running script in each container"
+sleep 5s
+
+temp_directory=$(mktemp -d)
+results_file="${temp_directory}/test-results.txt"
 green="\033[0;32m"
 red="\033[0;31m"
 no_color="\033[0m"
 script_file="git-openssl.sh"
-results_file="test-results.txt"
 [ -f "${results_file} "] || rm "${results_file}"
 for ubuntu_version in "${ubuntu_versions[@]}"; do
-  dockerfile="ubuntu-${ubuntu_version//\.}"
   ubuntu_container="ubuntu:${ubuntu_version}-with-sudo"
-  echo -e "${green}Building image... from file ${dockerfile} to create ${ubuntu_container}${no_color}"
-  docker build -t "${ubuntu_container}" -f "dockerfiles/${dockerfile}" ./dockerfiles
+  echo "Testing with ${ubuntu_container}"
   docker run -v "$(pwd):/src" --rm --name "git-openssl-shellscript-test" "ubuntu:${ubuntu_version}-with-sudo" /bin/bash -c "/src/${script_file} --skip-tests"
   if [ "${?}" -ne 0 ]; then
     # echo "${red}Failed on ubuntu:${ubuntu_version}${no_color}" | tee -a "${results_file}"
@@ -27,5 +46,3 @@ done
 printf "\nFinal Results:\n"
 cat "${results_file}"
 echo -e "${no_color}"
-#  docker run -v /path/to/sample_script.sh:/sample_script.sh \
-#   --rm ubuntu bash sample_script.sh
