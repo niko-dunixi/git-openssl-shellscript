638a151b4798cffab8ae6fd602e711fad47b42fa
Paul Baker <paul.nelson.baker@gmail.com>
2019-12-02 10:20:54 -0700

    Script to build docker image and test the git static binary

---
+++ build-release-image.sh
@@ -0,0 +1,17 @@
+#!/usr/bin/env bash
+set -e
+
+repository='paulnelsonbaker'
+name='git-static-binary'
+test_clone_url="https://github.com/paul-nelson-baker/git-openssl-shellscript.git"
+
+set -x
+docker build . -t "${name}:latest"
+
+clone_directory="${HOME}/$(basename $(mktemp -d 2>/dev/null || mktemp -d -t 'mytmpdir'))"
+mkdir -p "${clone_directory}"
+docker run -v "${clone_directory}:/clone" -w '/clone' "${name}:latest" clone "${test_clone_url}"
+
+git_version=$(docker run "${name}:latest" --version | awk '{ print $3 }')
+docker tag "${name}:latest" "${repository}/${name}:${git_version}"
+docker push "${repository}/${name}:${git_version}"
