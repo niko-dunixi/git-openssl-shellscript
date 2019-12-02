72f438443c63b9cc103ae6552846513c1b7abc87
Paul Baker <paul.nelson.baker@gmail.com>
2019-12-02 10:33:13 -0700

    Adding a note to self

--- build-release-image.sh
+++ build-release-image.sh
@@ -15,3 +15,6 @@ docker run -v "${clone_directory}:/clone" -w '/clone' "${name}:latest" clone "${
 git_version=$(docker run "${name}:latest" --version | awk '{ print $3 }')
 docker tag "${name}:latest" "${repository}/${name}:${git_version}"
 docker push "${repository}/${name}:${git_version}"
+
+# Notes, still can't get curl to statically compile into git....
+# Might need something like this? https://scripter.co/nim-deploying-static-binaries/
